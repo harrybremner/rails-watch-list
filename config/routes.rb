718447f4cdbadb07d7413847940398b6e1c60e5e
Harry Bremner <harrybremner@gmail.com>
2022-05-27 12:47:30 +0100

    added index as root

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,9 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
-  resources :lists do
+  root "lists#index"
+
+  resources :lists, except: :index do
     resources :movies, only: [:new, :create]
     resources :bookmarks, only: [:new, :create]
   end
